5ce3fbf928abaea6a8cb09b14319e4df7d635cee
Ranieri Althoff <ranisalt@gmail.com>
2018-04-30 22:42:53 -0300

    Replace ava with mocha, drop babel entirely

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const test = require('ava')
+const _assert = require('assert')
 const {Benchmark} = require('./')
 
 const timeout = 10
@@ -6,36 +6,44 @@ const timeout = 10
 // needed +1 to fix skewed timer loop
 const sleep = delay => () => new Promise(resolve => setTimeout(resolve, delay + 1))
 
-test('run the function for given time', async t => {
-  const bench = new Benchmark('test', sleep(timeout * 2))
-  const stats = await bench.run(timeout)
-  t.is(stats.length, 1)
-  t.true(stats[0] >= (timeout * 2000))
-})
+describe('Benchmark', () => {
+  const assert = _assert.strict || _assert
 
-test('call the passed function', async t => {
-  let called = false
-  const bench = new Benchmark('test', async sleep => {
-    called = true
-    await sleep()
-  }, sleep(timeout))
-  await bench.run(timeout)
-  t.true(called)
-})
+  it('run the function for given time', () => {
+    const bench = new Benchmark('it', sleep(timeout * 2))
+    return bench.run(timeout).then(stats => {
+      assert.equal(1, stats.length)
+      assert.ok(stats[0] >= (timeout * 2000))
+    })
+  })
 
-test('return elapsed time array', async t => {
-  const bench = new Benchmark('test', sleep(1))
-  const stats = await bench.run(timeout)
-  t.true(Array.isArray(stats))
-  t.true(typeof stats[0] === 'number')
-})
+  it('call the passed function', () => {
+    let called = false
+    const bench = new Benchmark('it', async sleep => {
+      called = true
+      await sleep()
+    }, sleep(timeout))
+    return bench.run(timeout).then(() => {
+      assert.ok(called)
+    })
+  })
 
-test('pass args to called function', async t => {
-  let called = 0
-  const bench = new Benchmark('test', async (num, sleep) => {
-    called = num
-    await sleep()
-  }, 42, sleep(timeout))
-  await bench.run(timeout)
-  t.is(called, 42)
+  it('return elapsed time array', () => {
+    const bench = new Benchmark('it', sleep(1))
+    return bench.run(timeout).then(stats => {
+      assert.ok(Array.isArray(stats))
+      assert.ok(typeof stats[0] === 'number')
+    })
+  })
+
+  it('pass args to called function', () => {
+    let called = 0
+    const bench = new Benchmark('it', async (num, sleep) => {
+      called = num
+      await sleep()
+    }, 42, sleep(timeout))
+    return bench.run(timeout).then(() => {
+      assert.equal(42, called)
+    })
+  })
 })
